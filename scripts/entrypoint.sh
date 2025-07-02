3f4e64e16aad285f538d9bdd66e1ea35f7048d52
sitharamsenarath <sitharamsenarath@gmail.com>
2025-07-02 13:42:26 +0200

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,33 +1,33 @@
-#!/bin/bash
-set -e
+# #!/bin/bash
+# set -e
 
-TAG=$1
+# TAG=$1
 
-# Login to GHCR non-interactively
-echo "$DOCKER_IMAGE" | docker login ghcr.io -u "$GITHUB_ACTOR" --password-stdin
+# # Login to GHCR non-interactively
+# echo "$DOCKER_IMAGE" | docker login ghcr.io -u "$GITHUB_ACTOR" --password-stdin
 
-echo "🔍 Detecting project type..."
+# echo "🔍 Detecting project type..."
 
-if [ -f "requirements.txt" ] && [ -f "main.py" ]; then
-    echo "Python project detected"
-    cat <<EOF > Dockerfile
-FROM python:3.9-slim
-WORKDIR /app
-COPY requirements.txt .
-RUN pip install --no-cache-dir -r requirements.txt
-COPY . .
-CMD ["python", "main.py"]
-EOF
-else
-    echo "Unknown project type. Exiting."
-    exit 1
-fi
+# if [ -f "requirements.txt" ] && [ -f "main.py" ]; then
+#     echo "Python project detected"
+#     cat <<EOF > Dockerfile
+# FROM python:3.9-slim
+# WORKDIR /app
+# COPY requirements.txt .
+# RUN pip install --no-cache-dir -r requirements.txt
+# COPY . .
+# CMD ["python", "main.py"]
+# EOF
+# else
+#     echo "Unknown project type. Exiting."
+#     exit 1
+# fi
 
-echo "Building image: ghcr.io/${GITHUB_REPOSITORY_OWNER}/${GITHUB_REPOSITORY##*/}:${TAG}"
-docker build -t ghcr.io/${GITHUB_REPOSITORY_OWNER}/${GITHUB_REPOSITORY##*/}:${TAG} .
+# echo "Building image: ghcr.io/${GITHUB_REPOSITORY_OWNER}/${GITHUB_REPOSITORY##*/}:${TAG}"
+# docker build -t ghcr.io/${GITHUB_REPOSITORY_OWNER}/${GITHUB_REPOSITORY##*/}:${TAG} .
 
-echo "Logging in to GitHub Container Registry"
-echo "${GITHUB_TOKEN}" | docker login ghcr.io -u "${GITHUB_ACTOR}" --password-stdin
+# echo "Logging in to GitHub Container Registry"
+# echo "${GITHUB_TOKEN}" | docker login ghcr.io -u "${GITHUB_ACTOR}" --password-stdin
 
-echo "Pushing image..."
-docker push ghcr.io/${GITHUB_REPOSITORY_OWNER}/${GITHUB_REPOSITORY##*/}:${TAG}
+# echo "Pushing image..."
+# docker push ghcr.io/${GITHUB_REPOSITORY_OWNER}/${GITHUB_REPOSITORY##*/}:${TAG}
